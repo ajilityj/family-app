8578996abc589ea89d2327ff0df62c8324043afb
ajilityj <aj@ajilitytechnologies.com>
2019-02-28 02:51:25 -0500

    setup token-based authentication

--- backend-rails/api/config/routes.rb
+++ backend-rails/api/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  post 'authenticate', to: 'authentication#authenticate'
   resources :conversations, only: [:index, :create]
   resources :messages, only: [:create]
   mount ActionCable.server => '/cable'
--- backend-rails/api/db/schema.rb
+++ backend-rails/api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_10_061729) do
+ActiveRecord::Schema.define(version: 2019_02_28_055724) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,14 @@ ActiveRecord::Schema.define(version: 2018_11_10_061729) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["conversation_id"], name: "index_messages_on_conversation_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "messages", "conversations"
